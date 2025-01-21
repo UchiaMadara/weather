c02f5e3ad3fc6e0c56f5a9d6dddd5014f9ba8b43
Selwyn <selwynv11@gmail.com>
2025-01-21 20:51:17 +0000

    index.html

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const weather = async () => {
   try {
-    const url = await fetch("https://goweather.herokuapp.com/weather/Curitiba");
+    const url = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+    );
     const data = await url.json();
     console.log(data);
   } catch {}
